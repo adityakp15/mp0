7293f5478cc8b3a60b8d294a5c7b08645afe5c96
adityakp15 <aditponnu@gmail.com>
2025-08-31 22:29:14 -0500

    Completed MP0

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
-/* Your JS here. */
-console.log('Hello World!')
+window.changeColor = function(color) {
+  document.querySelector('.box').style.backgroundColor = color;
+}
+
+const redButton = document.querySelector('.red');
+redButton.addEventListener('click', () => window.changeColor('red'));
+
+const blueButton = document.querySelector('.blue');
+blueButton.addEventListener('click', () => changeColor('blue'));
+
+const greyButton = document.querySelector('.grey');
+greyButton.addEventListener('click', () => changeColor('grey'));
